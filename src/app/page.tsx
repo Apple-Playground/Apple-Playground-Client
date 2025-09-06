5950cd870816ce211e9ba39f92586a151526f0d4
Dino0204 <s24020@gsm.hs.kr>
2025-09-06 23:44:09 +0900

    feat: animate background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import { AppleIcon } from "lucide-react";
 import SignInButton from "@/entities/signin-button/ui";
+import { BubbleBackground } from "@/shared/ui/bubble";
 
 export default function Home() {
   return (
-    <main className="flex justify-center items-center bg-black min-h-screen py-8 px-4">
-      <div className="flex flex-col items-center justify-center max-w-112 text-white text-center">
+    <BubbleBackground className="flex justify-center items-center bg-black min-h-screen py-8 px-4">
+      <div className="flex flex-col items-center justify-center max-w-md text-white text-center z-20">
         <div className="p-4 mb-8 rounded-2xl bg-gray-800">
           <AppleIcon width={64} height={64} />
         </div>
@@ -16,6 +17,6 @@ export default function Home() {
         </h3>
         <SignInButton />
       </div>
-    </main>
+    </BubbleBackground>
   );
 }
