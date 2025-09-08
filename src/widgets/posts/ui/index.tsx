60b167e0f5e23f2c7248932af52d4b4e51a86ba0
Dino0204 <s24020@gsm.hs.kr>
2025-09-09 03:08:02 +0900

    feat: posts

--- src/widgets/posts/ui/index.tsx
+++ src/widgets/posts/ui/index.tsx
@@ -1,17 +1,55 @@
-import { Post } from "@/entities/post/ui"
+"use client";
 
-export const Posts = () => {
+import Autoplay from "embla-carousel-autoplay";
+import { use } from "react";
+import type { Post as PostType } from "@/entities/post/model";
+
+import { Post } from "@/entities/post/ui";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/shared/ui/carousel";
+
+type PostsProps = {
+  posts: Promise<PostType[]>;
+};
+
+export const Posts = ({ posts }: PostsProps) => {
+  const postsData = use(posts);
   return (
-    <div className="grid mt-8 gap-8 max-sm:grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-5">
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-    </div>
-  )
-}
+    <>
+      <h2 className="text-spring-green-55 font-pixel font-bold text-2xl">
+        Featured
+      </h2>
+      {/* <Carousel opts={{ loop: true }} plugins={[Autoplay({ delay: 10000 })]}>
+        <CarouselPrevious />
+        <CarouselContent>
+          <CarouselItem className="basis-1/3">
+            <Post />
+          </CarouselItem>
+          <CarouselItem className="basis-1/3">
+            <Post />
+          </CarouselItem>
+          <CarouselItem className="basis-1/3">
+            <Post />
+          </CarouselItem>
+          <CarouselItem className="basis-1/3">
+            <Post />
+          </CarouselItem>
+          <CarouselItem className="basis-1/3">
+            <Post />
+          </CarouselItem>
+        </CarouselContent>
+        <CarouselNext />
+      </Carousel> */}
+      <div className="grid mt-8 gap-8 ">
+        {postsData.map((post) => (
+          <Post key={post.id} post={post} />
+        ))}
+      </div>
+    </>
+  );
+};
