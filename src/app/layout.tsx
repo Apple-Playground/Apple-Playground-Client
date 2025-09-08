95395cfc8c380f4612b1603973c20b8806a08b07
Dino0204 <s24020@gsm.hs.kr>
2025-09-09 03:06:19 +0900

    fix: themeProvider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-import { ThemeProvider } from "@/entities/provider/theme-provider";
+import { ThemeProvider } from "@/app/_providers/theme-provider";
 import { Toaster } from "@/shared/ui/sonner";
 
 const geistSans = Geist({
