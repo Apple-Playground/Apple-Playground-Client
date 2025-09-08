6985d60e4ceaaaa42b1276ad278b63c1c519a8cd
Dino0204 <s24020@gsm.hs.kr>
2025-09-09 03:05:45 +0900

    feat: accessToken

--- src/auth.ts
+++ src/auth.ts
@@ -1,4 +1,5 @@
 import { SupabaseAdapter } from "@auth/supabase-adapter";
+import jwt from "jsonwebtoken";
 import NextAuth from "next-auth";
 import type { Provider } from "next-auth/providers";
 import GitHub from "next-auth/providers/github";
@@ -17,9 +18,25 @@ export const providerMap = providers.map((provider) => {
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [GitHub],
   adapter: SupabaseAdapter({
-    url: process.env.SUPABASE_URL!,
-    secret: process.env.SUPABASE_SERVICE_ROLE_KEY!,
+    url: process.env.NEXT_PUBLIC_SUPABASE_URL ?? "",
+    secret: process.env.NEXT_PUBLIC_SUPABASE_SERVICE_ROLE_KEY ?? "",
   }),
+  callbacks: {
+    async session({ session, user }) {
+      const signingSecret = process.env.NEXT_PUBLIC_SUPABASE_JWT_SECRET;
+      if (signingSecret) {
+        const payload = {
+          aud: "authenticated",
+          exp: Math.floor(new Date(session.expires).getTime() / 1000),
+          sub: user.id,
+          email: user.email,
+          role: "authenticated",
+        };
+        session.supabaseAccessToken = jwt.sign(payload, signingSecret);
+      }
+      return session;
+    },
+  },
   pages: {
     signIn: "/",
     signOut: "/",
