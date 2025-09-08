98201124feec049172dd050a7c7c799a5ee8f7ba
Dino0204 <s24020@gsm.hs.kr>
2025-09-09 03:07:46 +0900

    fix: main

--- src/app/main/page.tsx
+++ src/app/main/page.tsx
@@ -1,12 +1,17 @@
+import { Suspense } from "react";
+import { getPosts } from "@/entities/post/api";
 import { Header } from "@/widgets/header/ui";
 import { Posts } from "@/widgets/posts/ui";
 
 export default function Main() {
+  const posts = getPosts();
   return (
     <>
       <Header />
       <main className="min-h-screen bg-grey-10 py-8 px-4">
-        <Posts />
+        <Suspense fallback={<div>Loading...</div>}>
+          <Posts posts={posts} />
+        </Suspense>
       </main>
     </>
   );
