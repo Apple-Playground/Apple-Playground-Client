3e51c93a121e176ee152281279c59cd7d4a33d20
Dino0204 <s24020@gsm.hs.kr>
2025-09-09 23:28:36 +0900

    fix: post

--- src/entities/post/ui/index.tsx
+++ src/entities/post/ui/index.tsx
@@ -9,8 +9,9 @@ import {
 import type { Post as PostType } from "../model";
 
 export const Post = ({ post }: { post: PostType }) => {
+
   return (
-    <Card className="relative overflow-hidden drop-shadow-xl p-4">
+    <Card className="flex p-4">
       <div className="aspect-[16/9] relative bg-black/85 rounded-2xl overflow-hidden">
         <Image
           src={"https://placehold.co/600x400?font=roboto"}
@@ -20,18 +21,47 @@ export const Post = ({ post }: { post: PostType }) => {
           className="object-cover"
         />
       </div>
-      <CardHeader className="p-0">
+      <CardHeader className="p-2">
         <CardTitle className="line-clamp-1 text-ellipsis">
           {post.title}
         </CardTitle>
-        <CardDescription className="line-clamp-3 text-ellipsis">
+        <CardDescription className="line-clamp-2 text-ellipsis">
           {post.description}
         </CardDescription>
       </CardHeader>
-      <CardContent className="flex justify-between text-xs p-0">
-        <p>{post.created_at}</p>
-        <p>{post.author_id}</p>
+      <CardContent className="flex justify-between p-2 text-xs">
+        <span>{post.created_at.split("T")[0]}</span>
+        <span>{post.author_id}</span>
       </CardContent>
     </Card>
   );
 };
+
+export const CarouselPost = ({ post }: { post: PostType }) => {
+  return (
+    <Card className="flex p-0 aspect-[16/9] relative overflow-hidden">
+      <Image
+        src={"https://placehold.co/600x400?font=roboto"}
+        alt="image"
+        fill
+        unoptimized
+        className="object-cover"
+      />
+      <div className="bg-black/40 absolute w-full h-full"></div>
+      <div className="absolute p-2 w-full bottom-0">
+        <CardHeader className="p-2">
+          <CardTitle className="line-clamp-1 text-ellipsis">
+            {post.title}
+          </CardTitle>
+          <CardDescription className="line-clamp-2 text-ellipsis">
+            {post.description}
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="flex justify-between p-2 text-xs">
+          <span>{post.created_at.split("T")[0]}</span>
+          <span>{post.author_id}</span>
+        </CardContent>
+      </div>
+    </Card>
+  );
+}
