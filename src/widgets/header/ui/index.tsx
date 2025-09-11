fbc2726e1541cd867771c233aa733c9090ac2955
Dino0204 <s24020@gsm.hs.kr>
2025-09-11 18:12:52 +0900

    chore: sidebar pointer

--- src/widgets/header/ui/index.tsx
+++ src/widgets/header/ui/index.tsx
@@ -4,7 +4,7 @@ import { SidebarTrigger } from "@/shared/ui/sidebar";
 export const Header = async () => {
   return (
     <header className="flex justify-between items-center pb-2 border-b-2">
-      <SidebarTrigger />
+      <SidebarTrigger className="cursor-pointer" />
       <h1 className="font-orbitron font-bold text-4xl">Home</h1>
       <div className="flex items-center gap-4">
         <ModeToggle />
