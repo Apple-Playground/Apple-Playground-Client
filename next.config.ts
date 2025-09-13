1a9dd983f82b8f311458c7754e47d078802c6818
Dino0204 <s24020@gsm.hs.kr>
2025-09-13 14:39:21 +0900

    feat: mdx

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
   experimental: {
     reactCompiler: true,
   },
+  pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
   images: {
     remotePatterns: [
       {
