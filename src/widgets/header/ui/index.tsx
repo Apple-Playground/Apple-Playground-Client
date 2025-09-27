6d662bd724432d826705348a95aa6ca25bbf14ea
Dino0204 <s24020@gsm.hs.kr>
2025-09-27 21:54:24 +0900

    feat: signout

--- src/widgets/header/ui/index.tsx
+++ src/widgets/header/ui/index.tsx
@@ -1,17 +1,19 @@
-"use client"
+"use client";
 
 import { usePathname } from "next/navigation";
+import SignOutButton from "@/entities/signout-button/ui";
 import { ModeToggle } from "@/shared/ui/mode-togle";
 import { SidebarTrigger } from "@/shared/ui/sidebar";
 
 export const Header = () => {
   const pageName = usePathname();
-  console.log(pageName);
+
   return (
     <header className="flex justify-between items-center mr-2 ml-2 mt-2 pb-2 border-b-2">
       <SidebarTrigger className="cursor-pointer" />
       <h1 className="font-orbitron font-bold text-4xl">{pageName}</h1>
       <div className="flex items-center gap-4">
+        <SignOutButton />
         <ModeToggle />
       </div>
     </header>
