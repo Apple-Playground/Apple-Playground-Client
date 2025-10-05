37eb7747f1074862aa56d25a584571057c39ff5c
Dino0204 <s24020@gsm.hs.kr>
2025-10-06 01:18:56 +0900

    chore: pageName

--- src/widgets/header/ui/index.tsx
+++ src/widgets/header/ui/index.tsx
@@ -11,7 +11,7 @@ export const Header = () => {
   return (
     <header className="flex justify-between items-center mr-2 ml-2 mt-2 pb-2 border-b-2">
       <SidebarTrigger className="cursor-pointer" />
-      <h1 className="font-orbitron font-bold text-4xl">{pageName}</h1>
+      <h1 className="font-orbitron font-bold text-4xl">{pageName.split("/")[1]}</h1>
       <div className="flex items-center gap-4">
         <SignOutButton />
         <ModeToggle />
